e929ed1665071ca4131bbbefd2a6178edaa758e8
mesaujjwal <47854573+mesaujjwal@users.noreply.github.com>
2019-02-23 18:12:34 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,59 +1,24 @@
 #include <stdio.h>
-
 int main()
-
 {
-
-int n, i, flag = 0;
-
-
-printf("Enter a positive integer: ");
-
-scanf("%d", n);
-
-
-for(i = 2; i <= n/2; ++i)
-
-{
-
-// condition for nonprime number
-
-if(n%i == 0)
-
-{
-
-flag = 1;
-
-//break;
-
+    int i, j, rows=5;
+    printf("Enter number of rows: ");
+    for(i=1; i<=rows; ++i)
+    int n=10, i;
+    unsigned long long factorial = 1;
+    printf("Enter an integer: ");
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else
+    {
+        for(j=1; j<=i; ++j)
+        for(i=1; i<=n; ++i)
+        {
+            printf("* ");
+            factorial *= i;            
+        }
+        printf("\n");
+        printf("Factorial of %d = %llu", n, factorial);
+    }
+    return 0;
 }
-
-}
-
-
-if (n = 1) 
-
-{
-
-printf("1 is neither a prime nor a composite number.");
-
-}
-
-else 
-
-{
-
-if (flag == 0)
-
-printf("%f is a prime number.", n);
-
-else
-
-printf("%d is not a prime number.", n);
-
-}
-
-
-// return 0;
-
-}​
