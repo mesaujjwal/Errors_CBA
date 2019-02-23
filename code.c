bfed0be21ef595e71a974a194751add4c75e4dd2
mesaujjwal <47854573+mesaujjwal@users.noreply.github.com>
2019-02-23 17:57:31 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,26 +1,24 @@
-language: cpp
-compiler: gcc
-dist: trusty
-
-before_install:
-# C++14
-  - sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  - sudo apt-get update -qq
-
-install: 
- # C++14
-  - sudo apt-get install -qq g++-6
-  - sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
- # cppcheck
-  - sudo apt-get install -qq cppcheck
-
- script: 
- # Build and run this project
-  - qmake
-  - make
-  - ./travis_qmake_gcc_cpp14_cppcheck
-        # cppcheck (mild)
-  - cppcheck --quiet . 
-        # cppcheck that makes the Travis build fail
-        #- cppcheck --quiet --error-exitcode=1 . 
-
+#include <stdio.h>
+int main()
+{
+    int i, j, rows=5;
+    printf("Enter number of rows: ");
+    for(i=1; i<=rows; ++i)
+    int n=10, i;
+    unsigned long long factorial = 1;
+    printf("Enter an integer: ");
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else
+    {
+        for(j=1; j<=i; ++j)
+        for(i=1; i<=n; ++i)
+        {
+            printf("* ");
+            factorial *= i;            
+        }
+        printf("\n");
+        printf("Factorial of %d = %llu", n, factorial);
+    }
+    return 0;
+}
