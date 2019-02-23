8964c932ddfe04dfa512e1a4f94e51b2296e43b6
mesaujjwal <47854573+mesaujjwal@users.noreply.github.com>
2019-02-23 18:25:29 +0530

    Update code.c

--- code.c
+++ code.c
@@ -18,7 +18,7 @@ int main()
     int* p1; // p1 can point to any location in memory
   int m = 100;
   p1 = m; // error
-  return 0;
+
     
     int* p1; // create a pointer to an integer
   int m = 100;
@@ -31,5 +31,29 @@ int main()
   int m = 100;
   p1 = &m;
   free(p1);//error - trying to free stack memory using free()
+  
+   int* p1;
+  if ((p1 = (int*)malloc(sizeof(int))) == NULL)
+  {
+    return 1;
+  }
+  *p1 = 99;
+  free(p1);
+  *p1 = 100; // BAD - undefined behavior
+  
+   char* str1 = (char*)malloc(strlen("Thunderbird") + 1);
+  strcpy_s(str1, strlen("Thunderbird") + 1, "Thunderbird");
+  //...
+  free(str1);  // first free
+         //...
+  free(str1); // double free
+  
+  malloc(sizeof(int))
+    
+     const char arr[] = "hello";
+  const char *cp = arr;
+  printf("Size of arr %lu\n", (int)sizeof(arr));
+  printf("Size of *cp %lu\n", (int)sizeof(*cp));
 
+  return 0;
 }
